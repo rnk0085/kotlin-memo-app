fff677ca8e85eeab74c2efd19955d8710cc64b0e
rnk0085 <rinawkmbi4k8@gmail.com>
2022-05-13 10:44:34 +0900

    MainActivityを修正

--- app/src/main/java/com/rnk0085/android/memo/MainActivity.kt
+++ app/src/main/java/com/rnk0085/android/memo/MainActivity.kt
@@ -6,12 +6,11 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
