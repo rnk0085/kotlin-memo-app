9018bd7228694cef36d19105028b5870d99852ce
rnk0085 <rinawkmbi4k8@gmail.com>
2022-05-20 23:32:12 +0900

    MemoRepositoryImplを実装

--- app/src/main/java/com/rnk0085/android/memo/repository/MemoRepositoryImpl.kt
+++ app/src/main/java/com/rnk0085/android/memo/repository/MemoRepositoryImpl.kt
@@ -2,19 +2,23 @@ package com.rnk0085.android.memo.repository
 
 import com.rnk0085.android.memo.database.datasource.MemoDataSource
 import com.rnk0085.android.memo.database.memo.Memo
+import com.rnk0085.android.memo.di.IoDispatcher
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class MemoRepositoryImpl @Inject constructor(
-    private val memoDataSource: MemoDataSource
+    private val memoDataSource: MemoDataSource,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
 ) : MemoRepository {
-    override suspend fun insert(memo: Memo) {
-        TODO("Not yet implemented")
+    override suspend fun insert(memo: Memo) = withContext(ioDispatcher) {
+        memoDataSource.insert(memo)
     }
 
     override fun getAllMemos(): Flow<List<Memo>> {
-        TODO("Not yet implemented")
+        return memoDataSource.getAllMemos()
     }
 }
