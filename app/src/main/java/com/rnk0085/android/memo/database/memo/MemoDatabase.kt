a40742501701720a3e537c07e7691f5845a49421
rnk0085 <rinawkmbi4k8@gmail.com>
2022-05-21 10:49:12 +0900

    DateConvertersをMemoDatabaseで使用

--- app/src/main/java/com/rnk0085/android/memo/database/memo/MemoDatabase.kt
+++ app/src/main/java/com/rnk0085/android/memo/database/memo/MemoDatabase.kt
@@ -2,8 +2,11 @@ package com.rnk0085.android.memo.database.memo
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.rnk0085.android.memo.utils.DateConverters
 
 @Database(entities = [Memo::class], version = 1)
+@TypeConverters(DateConverters::class)
 abstract class MemoDatabase : RoomDatabase() {
     abstract fun memoDao(): MemoDao
 }
